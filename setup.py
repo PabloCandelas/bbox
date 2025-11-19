cf542ce62d0648f7af9a9ee4308237e0759527ec
PabloCandelas <pcandelas98@gmail.com>
2025-11-19 11:52:09 +0100

    added video node, modified package.xml and setup.py to accept launch and node entries

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'bbox'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),        
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch')),        
+        (os.path.join('share', package_name, 'config'), glob('config/*.config.yaml')),
+        (os.path.join('share', package_name, 'lib'), glob('*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,7 +30,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'dummy = bbox.dummy:main'
+            'dummy = bbox.dummy:main',
+            'video = bbox.video:main'
         ],
     },
 )
